669f6be23381ec0815d7159a4152a89039b673e1
amer <amermuhamedamer@gmail.com>
2025-06-25 17:34:24 +0300

    Add the final workouts

---
+++ src/main/java/com/abdoatiia542/GraduationProject/model/enumerations/BodyFocus.java
@@ -0,0 +1,9 @@
+package com.abdoatiia542.GraduationProject.model.enumerations;
+public enum BodyFocus {
+    CHEST_MUSCLES,
+    ARM_SHOULDER,
+    BACK_MUSCLES,
+    LEG_BUTT,
+    SIX_PACK,
+    CARDIO
+}

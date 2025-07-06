bf3eb52332d3db28f3edbba3536f5ab589819472
amer <amermuhamedamer@gmail.com>
2025-07-07 00:13:25 +0300

    Add meal progress tracking: integrate CompletedMeal model and repository, update DailyProgress for nutrition and add endpoint for recording meal completion.

--- src/main/java/com/abdoatiia542/GraduationProject/repository/food/MealPlanRepository.java
+++ src/main/java/com/abdoatiia542/GraduationProject/repository/food/MealPlanRepository.java
@@ -1,7 +1,13 @@
 package com.abdoatiia542.GraduationProject.repository.food;
 
+import com.abdoatiia542.GraduationProject.model.Trainee;
 import com.abdoatiia542.GraduationProject.model.food.MealPlan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 public interface MealPlanRepository extends JpaRepository<MealPlan, Long> {
+    Optional<MealPlan> findTopByTraineeOrderByIdDesc(Trainee trainee);
+
 }

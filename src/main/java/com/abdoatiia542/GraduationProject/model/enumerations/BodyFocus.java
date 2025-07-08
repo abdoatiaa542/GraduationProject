6dfef363e0027f34efd92827ba21a3919d41c2a3
ABDELRAHMAN-ABD\Abdelrahman Abdallah <abdelrahman.abdallah@isoft.ae>
2025-07-08 11:21:38 +0300

    test deployment

--- src/main/java/com/abdoatiia542/GraduationProject/model/enumerations/BodyFocus.java
+++
@@ -1,9 +0,0 @@
-//package com.abdoatiia542.GraduationProject.model.enumerations;
-//public enum BodyFocus {
-//    CHEST_MUSCLES,
-//    ARM_SHOULDER,
-//    BACK_MUSCLES,
-//    LEG_BUTT,
-//    SIX_PACK,
-//    CARDIO
-//}
